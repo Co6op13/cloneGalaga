252f206e83060988718b9de0c79fec1e15b31edc
Co6op13 <Co6op13@yandex.ru>
2020-01-16 17:56:14 +0500

    небольшие испрвалния enemy, redplasma

--- Assets/Script/BlueEnemyMove.cs
+++ Assets/Script/BlueEnemyMove.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class BlueEnemyMove : MonoBehaviour
 {
     public float speed = 8;
+    public int damagCollision = 5;
     public Vector3 move = new Vector3(0, -1, 0);
     Rigidbody2D rb;
+     public string[] targetTags = {"Player"};
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         transform.Rotate(0, 0f, 180f);
         rb.velocity = move * speed;
+    }
+
+   
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        foreach(string currentTag in targetTags)
+			{	
+				if(currentTag == coll.transform.tag)
+				{
+				Debug.Log("-------------------");		
+				coll.transform.GetComponent<HeroHP>().AddDamage(damagCollision);
+				Destroy(gameObject);
+				}
+			}
     }
 
 
--- Assets/Script/Hero1Control.cs
+++ Assets/Script/Hero1Control.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class Hero1Control : MonoBehaviour
 {
     public float speed = 5;
+    private Camera cam;
+    private float topCam,downCam;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        cam = GetComponent<Camera>(); //Камера на основе которой будем определять вышел ли объект за ее границы
+        Vector3 point = cam.WorldToViewportPoint(transform.position); //Записываем положение объекта к границам камеры, X и Y это будут как раз верхние и нижние границы камеры
+        if(point.y < 0f || point.y > 1f || point.x > 1f || point.x < 0f) { //Если объект вышел за одну из этих границ, тут именно 0 и 1 это границы камеры. Но можно выйти и за эти значения, если указать например -0.2f
+    //нужный код
+}
+
+
+
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
-        rb.velocity = move * speed;
+        Vector3 point = cam.WorldToViewportPoint(transform.position); //Записываем положение объекта к границам камеры, X и Y это будут как раз верхние и нижние границы камеры
+        if(point.y < 0f || point.y > 1f || point.x > 1f || point.x < 0f)
+         { //Если объект вышел за одну из этих границ, тут именно 0 и 1 это границы камеры. Но можно выйти и за эти значения, если указать например -0.2f
+            //нужный код
+
+            Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+            rb.velocity = move * speed;
+        }
     }
 }
